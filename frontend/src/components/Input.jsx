d4d857fc971ec9a9181d0927a67d8035cbc5d8f6
rai <rai.conslog@hotmail.com>
2026-08-28 15:26:10 -0300

    add: implementa requisição do frontend

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -1,7 +1,9 @@
-export function TextInput({ placeholder }){
+export function TextInput({ placeholder, value, func }){
     return (
         <input 
             type="text" 
+            value={value}
+            onChange={func}
             placeholder={placeholder}
             className="w-full p-2 bg-secondary rounded placeholder:text-text-muted/50 focus:outline-none text-slate-200"
             />
--- frontend/src/pages/CreateUser.jsx
+++ frontend/src/pages/CreateUser.jsx
@@ -1,11 +1,44 @@
+import { useState } from "react"
 import { Button } from "../components/Button"
 import { TextInput } from "../components/Input"
 
 export function CreateUser(){
+    const[nickname, setNickname] = useState("")
+
+    const handleChange = (e) => {
+        setNickname(e.target.value)
+    }
+
+    async function handleCreateUser() {
+        try {
+            const response = await fetch('/api/user/create', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    'name': nickname
+                })
+            })
+            const user = await response.json()
+            sessionStorage.setItem('user_id', user.id)
+            window.location.href = '/create'
+        } catch(err) {
+            console.log("Error: ", err)
+        }
+
+    }
     return (
         <>
-            <TextInput placeholder={"Choose a nickname"}/>
-            <Button text={"Avançar"}/>
+            <TextInput 
+                placeholder={"Choose a nickname"}
+                value={nickname}
+                func={handleChange}
+                />
+            <Button 
+                text={"Avançar"}
+                onClick={handleCreateUser}
+                />
         </>
     )
 }
