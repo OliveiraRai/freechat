cd67b20a080361747ff205c30b1c809589273178
rai <rai.conslog@hotmail.com>
2026-09-03 22:43:05 -0300

    add: implementa chat TODO websockets

--- frontend/src/components/ChatBody.jsx
+++ frontend/src/components/ChatBody.jsx
@@ -4,46 +4,67 @@ import { TextInput } from "./Input"
 import { useNavigate } from "react-router-dom"
 
 export function ChatBody(){
-    navigate = useNavigate();
+    const navigate = useNavigate();
     const[chatId, setChatId] = useState(null);
     const[chatCode, setChatCode] = useState(null);
     const[userId, setUserId] = useState(null);
     const[userName, setUserName] = useState('');
-    const[connected, setConnected] = useState(False)
+    const[connected, setConnected] = useState(false);
+    const[message, setMessage] = useState('');
 
     useEffect(() => {
-        chat_id = sessionStorage.getItem('chat_id');
-        chat_code = sessionStorage.getItem('chat_code');
-        user_id = sessionStorage.getItem('user_id');
+        const chat_id = sessionStorage.getItem('chat_id');
+        const chat_code = sessionStorage.getItem('chat_code');
+        const user_id = sessionStorage.getItem('user_id');
         if(chat_id && chat_code && user_id){
-            setChatId = chat_id;
-            setChatCode = chat_code;
-            setUserId = user_id;
+            setChatId(chat_id)
+            setChatCode(chat_code)
+            setUserId(user_id)
+            getName(userId)
         } else {
             console.log("Error: chat and user data missing");
-            Navigate("/");
+            navigate("/");
         }
-        async function getName() {
+        async function getName({currentUserId}) {
             try{
-                const response = await fetch(`api/user/${userId}`);
-                const data = response.json();
+                const response = await fetch(`api/user/${currentUserId}`);
+                const data = await response.json();
                 setUserName = data.name;
             }catch(err){
                 console.log("Error while trying to get user name: ", err);
             }
         }
-    })
+    }, [navigate])
 
+    const handleChange = (event) => {
+        setMessage(event.target.value);
+    };
 
+    const handleChatDelete = async () => {
+        try {
+            const response = await fetch(`/api/chat/${chatId}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+            if(response.ok){
+                console.log("Chat successfully deleted.")
+            }
+        }catch(err){
+            console.log("Error while deleting chat: ", err)
+        }
+    };
 
-    const handleChatDelete = () => {
-        // TODO
-    }
+    const handleMessage = () => {
+        console.log(message);
+        setMessage('');
+    };
 
     return (
         <div className="bg-primary h-screen overflow-hidden">
             <TopBar
-                connected={}
+                connected={connected}
                 userName={userName}
                 onDelete={handleChatDelete}
             />
@@ -55,9 +76,14 @@ export function ChatBody(){
                         </div>
                     </div>
                     <div className="flex justify-between py-2 gap-2 px-2">
-                        <TextInput placeholder={"Type your message"}/>
+                        <TextInput 
+                            placeholder={"Type your message"}
+                            func={handleChange}
+                            value={message}
+                            />
                         <button 
                             className="w-10 rounded-full focus:outline-none bg-tertiary cursor-pointer text-slate-200/80"
+                            onClick={handleMessage}
                             >
                             +
                         </button>
