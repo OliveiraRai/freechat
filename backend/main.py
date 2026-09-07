e47a7d29e1071a6a11a644075ec4815a62ad0883
rai <rai.conslog@hotmail.com>
2026-09-07 16:42:29 -0300

    update: modifica main.py

--- backend/main.py
+++ backend/main.py
@@ -16,8 +16,9 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://vercel.app",
+    allow_origins=["https://freechat-didins96y-rai-oliveiras-projects.vercel.app/",
                    "http://localhost:8000",
+                   "http://localhost:5173",
                    "http://127.0.0.1:5173"
                    ], 
     allow_credentials=True,
