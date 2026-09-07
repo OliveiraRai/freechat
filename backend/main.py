fd066b8a7583309a5da2cf677ec7726e2714fd7a
rai <rai.conslog@hotmail.com>
2026-09-07 17:16:00 -0300

    fix: ajusta CORS

--- backend/main.py
+++ backend/main.py
@@ -22,10 +22,11 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://freechat-rai-oliveiras-projects.vercel.app",
-                   "http://localhost:8000",
-                   "http://localhost:5173",
-                   "http://127.0.0.1:5173"
+    allow_origins=[
+        "https://freechat-virid.vercel.app",
+        "http://localhost:8000",
+        "http://localhost:5173",
+        "http://127.0.0.1:5173"
                    ], 
     allow_credentials=True,
     allow_methods=["*"],
