2bb45e21b5fcdec25b32adcfa5f4ccec724a765c
rai <rai.conslog@hotmail.com>
2026-09-07 17:26:33 -0300

    add: erro ao apagar dado null

--- backend/main.py
+++ backend/main.py
@@ -148,9 +148,12 @@ def DeleteChat(chat_id: int, user_id: int, session: Session = Depends(get_sessio
     user = session.get(models.User, user_id)
     if user:
         session.delete(user)
-    guest = session.get(models.User, chat.guest_id)
-    if guest:
-        session.delete(guest)  
+    try:
+        guest = session.get(models.User, chat.guest_id)
+        if guest:
+            session.delete(guest)  
+    except TypeError or AttributeError:
+        print("Error while deleting guest data.")
         
     
     session.commit()
